f636991294f1c30dd41cb4a94691a67d6d83f73f
Marcello Seri <marcello.seri@gmail.com>
2015-02-19 23:15:34 +0000

    Fixed

--- endpoints/process.js
+++ endpoints/process.js
@@ -14,7 +14,7 @@ var ocr    = require('../lib/ocr')
     , auth = require('../lib/auth');
 
 // how can I cange the location?
-var location    = path.resolve(".") + "/tmp/"
+var location    = null
     , imageName = null;
 
 // check if sha1 fields is present
@@ -41,32 +41,33 @@ var multer_mw = multer({ dest: location,
     if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpg' && file.mimetype !== 'image/jpeg') {
       return false;
     }
-    console.log(file.originalname + ' is starting ...');
+    log(file.originalname + ' is starting ...');
   },
   onFileUploadData: function (file, data) {
-    console.log(data.length + ' of ' + file.fieldname + ' arrived')
+    //console.log(data.length + ' of ' + file.fieldname + ' arrived')
   },
   onFileUploadComplete: function (file) {
-    console.log(file.fieldname + ' uploaded to  ' + file.path);
+    log(file.fieldname + ' uploaded to  ' + file.path);
+    location = file.path
   },
   onParseStart: function () {
-    console.log('Form parsing started at: ', new Date())
+    //console.log('Form parsing started at: ', new Date())
   },
   onParseEnd: function (req, next) {
-    console.log('Form parsing completed at: ', new Date());
+    //console.log('Form parsing completed at: ', new Date());
     next(); //call the next middleware, if next() is not called then request will hang and no response will be returned
   },
   onFileSizeLimit: function (file) {
-    console.log('Failed: ', file.originalname);
+    logError('Failed: ', file.originalname);
   },
   onFilesLimit: function () {
-    console.log('Crossed file limit!')
+    logError('Crossed file limit!')
   },
   onFieldsLimit: function () {
-    console.log('Crossed fields limit!')
+    logError('Crossed fields limit!')
   },
   onPartsLimit: function () {
-    console.log('Crossed parts limit!')
+    logError('Crossed parts limit!')
   },
   onError: function (error, next) {
     logError(error);
@@ -77,22 +78,30 @@ var multer_mw = multer({ dest: location,
 // process the file and return the total
 // or error 401 in case of error
 function get_total(req, res, next) {
-  ocr.process(location + imageName, function(err, total) {
-    if (err) {
-      log("Unable to get total from ", imageName);
-      logError(err);
-      res.end(err, 401);
-    } else {
-      log('OCR total:', total);
-      var success = {
-        sha1: imageName,
-        total: total
-      }; 
-      log('Success: ', success);
-      res.status(200);
-      res.json(success);
-    }
-  });
+  
+  if (location) {
+    ocr.process(location, function(err, total) {
+      if (err) {
+        log("Unable to get total from ", imageName);
+        logError(err);
+        res.status(401);
+        res.end(err);
+      } else {
+        log('OCR total:', total);
+        var success = {
+          sha1: imageName,
+          total: total
+        }; 
+        log('Success: ', success);
+        res.status(200);
+        res.json(success);
+      }
+    });
+  } else {
+    logError("File location not received");
+    res.status(401);
+    res.end('File location not received');
+  }
 }
 
 // this is the route handler. First check auth. If so, 
--- tests/upload_and_process.test.js
+++ tests/upload_and_process.test.js
@@ -15,6 +15,8 @@ var sha1Image = "748fa97bd26133c7b0f514fb68f4ba394bad46b200a777cbdab90dd0a0b0799
 
 var app = require('../app');
 
+// definitely not idiomatic or nice but it does the trick
+
 request(app)
   .post('/upload')
   .field('Content-Type', 'multipart/form-data')
@@ -24,9 +26,20 @@ request(app)
   .expect(200)
   .end(function (err, res) {
       var form = res.body;
-      console.log(form);
-      
-      if (err) {
-        console.log(err);
+      if (form.sha1 == '748fa97bd26133c7b0f514fb68f4ba394bad46b200a777cbdab90dd0a0b0799d' && form.total=='120.60') {
+        log("Image succesfully uploaded and processed!");
       }
+      if (err) {
+        log("Error: " + err);
+      }
+  });
+
+request(app)
+  .post('/upload')
+  .end(function (err, res) {  
+    if (res.status == 401) {
+      log("Success!");
+    } else {
+      log("Error...");
+    }
   });
